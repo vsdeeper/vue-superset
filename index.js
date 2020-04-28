2f68ff4da5f8779cfba217c2e45a9dbe2c751d8c
chenshuai <chenshuai@e-buychina.com.cn>
2020-04-28 16:00:55 +0800

    perf(build): 打包发布逻辑优化

---
+++ index.js
@@ -0,0 +1,12 @@
+import { config as Config } from './dist/config'
+import Common, { weixin as Weixin } from './dist/common'
+import Mobile from './dist/mobile'
+import Pc from './dist/pc'
+
+export {
+  Config,
+  Common,
+  Weixin,
+  Mobile,
+  Pc
+}
--- scripts/release.js
+++ scripts/release.js
@@ -2,14 +2,9 @@ const shell = require('shelljs')
 
 // 删除dist目录
 shell.rm('-rf', 'dist')
-// shell.rm('-rf', 'common')
-// shell.rm('-rf', 'mobile')
-// shell.rm('-rf', 'pc')
-// shell.rm('-rf', 'config')
-// shell.rm('-rf', 'lang')
 
 // 创建dist目录
 shell.mkdir('-p', 'dist')
 
-// 向dist目录复制文件
-shell.cp('-Rf', 'src/superset/*', 'dist')
+// 复制
+shell.cp('-Rf', ['src/superset/common/', 'src/superset/config/', 'src/superset/lang/', 'src/superset/mobile/', 'src/superset/pc/'], 'dist')
