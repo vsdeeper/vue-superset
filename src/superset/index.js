fcefa8d6fbeeb4f2c004001de4c612533f2f2d21
chenshuai <chenshuai@e-buychina.com.cn>
2020-05-06 17:44:58 +0800

    feat(ImgVerifyCode): 图片验证码组件引用更新

--- src/superset/index.js
+++ src/superset/index.js
@@ -2,11 +2,13 @@ import { config as Config } from './config'
 import Common, { weixin as Weixin } from './common'
 import Mobile from './mobile'
 import Pc from './pc'
+import { ImgVerifyCode } from './pc/components'
 
 export {
   Config,
   Common,
   Weixin,
+  ImgVerifyCode,
   Mobile,
   Pc
 }
